9bfae04072e69a58261ceb9a471bb14dd1565962
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-02-18 16:22:42 +0900

    cocoapodsのインストール

---
+++ LifeIsFood/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  LifeIsFood
+//
+//  Created by 福島大稀 on 2021/02/18.
+//
+
+import UIKit
+
+class CalendarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-LifeIsFood-LifeIsFoodUITests/Pods-LifeIsFood-LifeIsFoodUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LifeIsFood_LifeIsFoodUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LifeIsFood_LifeIsFoodUITests
+@end
